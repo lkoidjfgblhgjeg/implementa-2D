7c37b49ee98df56a2eabf69c66f56b3d635cf9f5
jessicaflorencio98 <com.jflorenciom24@alumnos.cecyteg.edu.mx>
2025-03-18 16:29:19 -0600

    practica 7.py

--- practica 7.py
+++ practica 7.py
@@ -1,6 +1,7 @@
-
+print("Ciudad","pais","comunidad",sep="\t")
 print("nombre","edad","ciudad",sep="\t")
 print("Ana","25","corea del sur",sep="\t")
 print("Mario","30","paris",sep="\t")
 print("Luisa","18","Honduras",sep="\t")
 holisss(como estas?)
+print("Mnago","Manzana","Durazno",sep="\t")
